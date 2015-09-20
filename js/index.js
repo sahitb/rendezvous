82f74c6ca89c7826c28e9653c0118c36ce6aab29
Chien Min Soh <sohchienmin@gmail.com>
2015-09-20 01:07:51 -0400

    changed to expedia api

---
+++ js/index.js
@@ -0,0 +1,143 @@
+var names = [];
+var airports = [];
+var destinations = [];
+var costs = [];
+var min_destination = 0;
+var groupMode = false;
+var clicked_Once = false;
+var bestsum = 1000000;
+var bestCoordinates;
+var best = 0;
+var homes = [];
+var dateDepart = 0;
+var dateReturn = 0; 
+
+
+function dateSubmitted(){
+	$('#noDate').css("display", "none");
+	$('#noDate2').css("display", "none");
+
+	if(document.getElementById("dateInput").value == ""){
+		$('#noDate').css("display", "block");
+	}
+	else if (document.getElementById("dateInput2").value == "") {
+		$('#noDate2').css("display", "block");
+	}
+	else if(document.getElementById("first_radio").checked == false && 
+		document.getElementById("second_radio").checked == false){
+		$('#noMode').css("display", "block");
+		$('#noDate').css("display", "none");
+	}
+	else{
+		if(document.getElementById("second_radio").checked == true){
+			console.log("groupmode is true");
+			groupMode = true;
+		}
+		$('#noDate').css("display", "none");
+		$('#noDate2').css("display", "none");
+		$('#noMode').css("display", "none");
+		$('.navbar').css("display", "none");
+		dateDepart = document.getElementById("dateInput").value;
+		dateReturn = document.getElementById("dateInput2").value;
+		console.log(dateDepart);
+		console.log(dateReturn);
+		$('#jump')[0].click();
+	}
+}
+
+function showFailure() 
+{
+   document.getElementById('failureAlert').style.display = "block";
+   document.getElementById('successAlert').style.display = "none";
+
+}
+
+function showSuccess() 
+{
+   document.getElementById('successAlert').style.display = "block";
+   document.getElementById('failureAlert').style.display = "none";
+}
+
+function decisionMode(){
+	if(clicked_Once){
+		if(destinations.length > 0){
+			localStorage.groupMode = groupMode;
+			localStorage.dateDepart = dateDepart;
+			localStorage.dateReturn = dateReturn;
+			localStorage.names = JSON.stringify(names);
+			localStorage.airports = JSON.stringify(airports);
+			localStorage.destinations = JSON.stringify(destinations);
+			
+			$("#loading_page")[0].click();
+		}
+		else{
+			$('#noDestination').css("display", "block");
+		}
+	}
+	else{
+		clicked_Once = true;
+		if(groupMode){
+			groupComplete();
+		}
+		else{
+			localStorage.groupMode = groupMode;
+			localStorage.dateDepart = dateDepart;
+			localStorage.dateReturn = dateReturn;
+			localStorage.names = JSON.stringify(names);
+			localStorage.airports = JSON.stringify(airports);
+			localStorage.destinations = JSON.stringify(destinations);
+			
+			$("#loading_page")[0].click();
+		}
+	}
+}
+
+function groupComplete()
+{
+	if(names.length == 0){
+		$('#noGroup').css("display", "block");
+	}
+	else{
+		$('#travellerForm').fadeOut('slow');
+		$('#travellerForm_Instructions').fadeOut('slow');
+		$('#destinationForm_Instructions').fadeIn('slow');
+		$('#destination_Form').fadeIn('slow');
+		$('#traveller_table').fadeOut('slow');
+		$('#destination_table').fadeIn('slow');
+		$('#group_Complete').html("Calculate My Trip!");
+	}
+}
+
+function travellerSubmitted()
+{
+	$('#noGroup').css('display', 'none');
+	if ((document.getElementById("travellerForm").elements[0].value == "") || 
+		(document.getElementById("travellerForm").elements[1].value == ""))
+	{
+		showFailure();
+	}
+	else
+	{
+		names.push(document.getElementById("travellerForm").elements[0].value);
+		airports.push(document.getElementById("travellerForm").elements[1].value);
+		showSuccess();
+		$('#travtable').append('<tr><td>' + names[names.length - 1] + '</td><td>' + airports[airports.length - 1] + '</td></tr>');
+		document.getElementById("travellerForm").reset();
+	}
+}
+
+function destinationSubmitted(){
+	if(document.getElementById("destination").value == ""){
+		$("#falseDestination").css("display", "block");
+	}
+	else{
+
+		destinations.push(document.getElementById("destination").value);
+		showSuccess();
+		$('#desttable').append('<tr><td>' + destinations[destinations.length - 1] + '</td></tr>');
+		document.getElementById("destination_Form").reset();
+	}
+	
+}
+// 
+
---
+++ js/loading.js
@@ -0,0 +1,182 @@
+var names = [];
+var airports = [];
+var destinations = [];
+var costs = [];
+var min_destination = 0;
+var groupMode = false;
+var clicked_Once = false;
+var bestsum = 1000000;
+var bestCoordinates;
+var best = 0;
+var homes = [];
+var dateDepart = 0;
+var dateReturn = 0; 
+var destinationsID = [];
+var ExpediaKey = "X8vOe6BbHkoqGOBlzVoJZCLAnYbAusCQ";
+
+// Function is called when loading.html is loaded. Decides which method to calculate.
+function decideCalculation(){
+	groupMode = localStorage.groupMode;
+	dateDepart = localStorage.dateDepart;
+	dateReturn = localStorage.dateReturn;
+	names = JSON.parse(localStorage.names);
+	airports = JSON.parse(localStorage.airports);
+  console.log(airports);
+	destinations = JSON.parse(localStorage.destinations);
+	console.log(destinations[0]);
+	if(groupMode){
+		calculateGroup();
+	}
+	else{
+		console.log("indi runs");
+		calculateIndividual();
+	}
+}
+
+// Calculates group to specific selected destinations
+function calculateGroup(){
+
+  for (var i = 0; i < destinations.length; i++) { 
+    (function(i) {
+      var data = destinations[i].toString();
+      data = data.replace('-', '');
+      //console.log(data);
+      $.getJSON("http://terminal2.expedia.com/x/suggestions/regions?", {query: data, apikey: ExpediaKey})
+        .done(function(json) { 
+          
+          destinationsID.push(json.sr[0].id);
+          if (destinationsID.length == destinations.length) {
+            getCosts();
+          }
+          
+        })
+        .fail(function() {
+            alert("Please reload the page!");
+            console.log("Expedia Suggestions API failed");
+        });
+    })(i);
+  }
+  /*$.each(destinations, function (i, item) {
+    console.log(item);
+    $.getJSON("http://terminal2.expedia.com/x/suggestions/regions?", {query: item, apikey: ExpediaKey})
+        .done(function(json) { 
+           console.log(i);
+           console.log(destinations.length);
+           destinationsID.push(json.sr[0].id);
+           if (i == destinations.length - 1) {
+            console.log(destinationsID);
+           }
+          
+        })
+        .fail(function() {
+            console.log("Expedia Suggestions API failed");
+        });
+  });
+*/
+}
+
+//Get costs from expedia api
+function getCosts(){
+  console.log(destinationsID);
+  console.log(airports);
+  for (var i = 0; i < airports.length; i++) {
+    (function(i) {
+        for (var j = 0; j < destinations.length; j++) {
+          (function(j) {
+           setTimeout(function(){
+            
+                var origin = airports[i].toString().substring(airports[i].length-3);
+                console.log("Origin:" + origin);
+                var destination = destinations[j].toString().substring(destinations[j].length-3);
+                console.log("Destination:" + destination);
+                var data = {
+                  departureDate: dateDepart,
+                  originAirport: origin,
+                  destinationAirport: destination,
+                  returnDate: dateReturn,
+                  regionid: destinationsID[j],
+                  adults: 1,
+                  limit: 1,
+                  apikey: ExpediaKey
+                }
+                $.getJSON("http://terminal2.expedia.com/x/packages?", data)
+                  .done(function(json) {
+
+                    var detail = {origin: origin, destination: destination, price: json.FlightList.Flight.FlightPrice.TotalRate.Value, name: names[i]};
+                    costs.push(detail);
+                    if (costs.length == airports.length * destinations.length) {
+                      calculateBestFlightV1();
+                    }
+                  })
+                  .fail(function() {
+                      alert("Please reload the page!");
+                  });
+            
+
+           }, 1000 * j);
+          })(j);
+
+        }
+    })(i);
+  }
+}
+
+function compare(a,b) {
+  if (a.destination < b.destination)
+    return -1;
+  if (a.destination > b.destination)
+    return 1;
+  return 0;
+}
+// Calculates best flight
+function calculateBestFlightV1() {
+  costs.sort(compare);
+
+  var count = 0;
+  for (var i = 0; i < destinations.length; i++) {
+    var sum = 0;
+    for (var j = 0; j < airports.length; j++) {
+      sum += parseFloat(costs[count].price);
+      count++;
+    }
+    if (sum < bestsum) {
+      best = i;
+      console.log(best);
+      bestsum = sum.toFixed(2); 
+    }
+  }
+  
+  for (var i = 0; i < costs.length; i++) {
+    if (String(costs[i].destination) != destinations[best].toString().substring(destinations[best].length-3)) {
+      costs.splice(i--,1);
+    }
+  }
+
+  var worldAirports;
+    $.getJSON("https://raw.githubusercontent.com/jbrooksuk/JSON-Airports/master/airports.json", function(json) { 
+        worldAirports = json;
+      $.each(worldAirports, function(k,airportItem){
+        for (var i = 0; i < airports.length; i++) {
+              if (airportItem.iata == airports[i].substring(airports[i].length-3)) {
+                var x = {lat: airportItem.lat, lon: airportItem.lon};
+
+                console.log(x);
+                console.log(typeof homes);
+                homes.push(x);
+              }
+          }
+          if (destinations[best].substring(destinations[best].length-3) == airportItem.iata) {
+            bestCoordinates = {lat: airportItem.lat, lon: airportItem.lon};
+            localStorage.bestCoordinates = JSON.stringify(bestCoordinates);
+            console.log(typeof localStorage.bestCoordinates);
+            
+          }
+
+      });
+      localStorage.best = best;
+      //debugger;
+      localStorage.costs = JSON.stringify(costs);
+      localStorage.homes = JSON.stringify(homes);
+      $('#results')[0].click();
+  });
+}
